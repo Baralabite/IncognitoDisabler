06d0b466ee3b76d24371912585ab850a121a934c
John <john@JOHN-UBUNTU-VIRTUALBOX.(none)>
2014-03-02 18:35:27 +1000

    Inital commit

---
+++ disable_incognito.sh
@@ -0,0 +1,15 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Incognito Mode Disabler"
+echo "Version: 1.0"
+echo "Author: John Board"
+echo "==============================\n"
+echo "Creating settings directory..."
+mkdir /etc/opt/chrome/policies/managed/ -p
+echo "Writing setting..."
+echo '{\n    "IncognitoModeAvailability": 1\n}' > /etc/opt/chrome/policies/managed/incognito_policy.json
+echo "Finished!"
+exit
